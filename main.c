1678482d0b8398045ed49d339a6eec3a20eb2c14
Jeriel <jeriel.verissimo@protonmail.com>
2020-07-04 14:01:48 -0300

    drawing a tiangle

--- main.c
+++ main.c
@@ -36,6 +36,63 @@ int main() {
 
   /* OTHER STUFF GOES HERE NEXT */
 
+  GLfloat points[] = {
+     0.0f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+  };
+
+  GLuint vbo = 0;
+  glGenBuffers (1, &vbo);
+  glBindBuffer (GL_ARRAY_BUFFER, vbo);
+  glBufferData (GL_ARRAY_BUFFER, sizeof (points), points, GL_STATIC_DRAW);
+
+  GLuint vao = 0;
+  glGenVertexArrays (1, &vao);
+  glBindVertexArray (vao);
+  glEnableVertexAttribArray (0);
+  glBindBuffer (GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  const char* vertex_shader = "#version 410\n"
+    "in vec3 vp;"
+    "void main () {"
+    "  gl_Position = vec4 (vp, 1.0);"
+    "}";
+
+  const char* fragment_shader = "#version 410\n"
+    "out vec4 frag_colour;"
+    "void main () {"
+    "  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+    "}";
+
+  GLuint vs = glCreateShader (GL_VERTEX_SHADER);
+  glShaderSource (vs, 1, &vertex_shader, NULL);
+  glCompileShader (vs);
+
+  GLuint fs = glCreateShader (GL_FRAGMENT_SHADER);
+  glShaderSource (fs, 1, &fragment_shader, NULL);
+  glCompileShader (fs);
+
+  GLuint shader_program = glCreateProgram ();
+  glAttachShader (shader_program, fs);
+  glAttachShader (shader_program, vs);
+  glLinkProgram (shader_program);
+
+  while (!glfwWindowShouldClose (window)) {
+    // wipe the drawing surface clear
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram (shader_program);
+    glBindVertexArray (vao);
+
+    // draw points 0-3 from the currently bound VAO with current in-use shader
+    glDrawArrays (GL_TRIANGLES, 0, 3);
+    // update other events like input handling
+    glfwPollEvents ();
+    // put the stuff we've been drawing onto the display 
+    glfwSwapBuffers (window);
+  }
+
   // close GL context and any other GLFW resources
   glfwTerminate();
   return 0;
