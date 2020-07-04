62a5ba5302ae6cf2bc13366dd9c38c47f87934e1
Jeriel <jeriel.verissimo@protonmail.com>
2020-07-04 14:41:53 -0300

    setup logs

--- main.c
+++ main.c
@@ -1,9 +1,86 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define GL_LOG_FILE "gl.log"
+
+bool restart_gl_log () {
+  FILE* file = fopen (GL_LOG_FILE, "w");
+
+  if (!file) {
+    fprintf (stderr, 
+             "ERROR: could not open GL_LOG_FILE  log file %s for writing\n",
+             GL_LOG_FILE);
+    return false;
+  }
+
+  time_t now = time (NULL);
+  char* date = ctime (&now);
+
+  fprintf (file, "GL_LOG_FILE log. local time %s\n", date);
+  fclose (file);
+  return true;
+}
+
+bool gl_log (const char* message, ...) {
+  va_list argptr;
+
+  FILE* file = fopen (GL_LOG_FILE, "a");
+
+  if (!file) {
+    fprintf (stderr, 
+             "ERROR: could not open GL_LOG_FILE  log file %s for writing\n",
+             GL_LOG_FILE);
+    return false;
+  }
+
+  va_start (argptr, message);
+  vfprintf (file, message, argptr);
+  va_end (argptr);
+  fclose(file);
+  return true;
+}
+
+bool gl_log_err (const char* message, ...) {
+  va_list argptr;
+
+  FILE* file = fopen (GL_LOG_FILE, "a");
+
+  if (!file) {
+    fprintf (stderr, 
+             "ERROR: could not open GL_LOG_FILE  log file %s for writing\n",
+             GL_LOG_FILE);
+    return false;
+  }
+
+  va_start (argptr, message);
+  vfprintf (file, message, argptr);
+  va_end (argptr);
+
+  va_start (argptr, message);
+  vfprintf (stderr, message, argptr);
+  va_end (argptr);
+
+  fclose (file);
+  
+  return true;
+}
+
+void gflw_error_callback (int error, const char* description) {
+  gl_log_err ("GLFW ERROR: code %i msg: %s\n", error, description);
+}
 
 int main() {
-  // start GL context and O/S window using GLFW
+
+  if (!restart_gl_log ()) { /* quit? */ }
+
+  // start GL context and O/S window using GLFW helper library
+  gl_log ("starting GLFW\n%s\n", glfwGetVersionString());
+  // register the error call-back function that we wrote, above
+  glfwSetErrorCallback (gflw_error_callback);
   if (!glfwInit()) {
     fprintf (stderr, "ERROR: could not start GLFW3\n");
     return 1;
